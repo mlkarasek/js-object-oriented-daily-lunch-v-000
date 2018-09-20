4c58c6f25dcb5a4c3d1120b3e8ddfe5ac9f7b2a6
Monica  Karasek <mlkarasek@gmail.com>
2018-09-20 19:04:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+const Neighborhood = (() => {
+   let neighborhoodIds = 1;
+   return class {
+     constructor(name) {
+       this.id = neighborhoodIds++;
+       this.name = name;
+       store.neighborhoods.push(this);
+     }
+
+     customers() {
+       return store.customers.filter(customer => customer.neighborhoodId === this.id);
+     }
